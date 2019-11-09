4f4088ae72ea681aa9e15205d3f35202fdf7ea08
Kevin Romero <kevin.romero@red4g.net>
2019-11-08 19:55:32 -0600

    series

--- graph3D.py
+++ graph3D.py
@@ -3,6 +3,8 @@ from sympy import Symbol, diff, limit, oo
 import matplotlib.pyplot as plt
 import numpy as np
 import sympy as sym
+import signal
+import math
 import os
 import re
 
@@ -14,11 +16,11 @@ def clear(): return os.system('cls')
 
 def draw():
     try:
-        print('Enter M:')
+        print('Enter M: ', end='')
         xval = parseInput('np')
-        print('Enter N:')
+        print('Enter N: ', end='')
         yval = parseInput('np')
-        print('Enter P:')
+        print('Enter P: ', end='')
         zval = parseInput('np')
         fig = plt.figure()
         ax = fig.gca(projection='3d')
@@ -39,11 +41,11 @@ def partial():
         x = Symbol('x')
         y = Symbol('y')
         z = Symbol('z')
-        print('Enter M:')
+        print('Enter M: ', end='')
         M = parseInput('sym')
-        print('Enter N:')
+        print('Enter N: ', end='')
         N = parseInput('sym')
-        print('Enter P:')
+        print('Enter P: ', end='')
         P = parseInput('sym')
         my = diff(eval(M), y)
         nx = diff(eval(N), x)
@@ -68,11 +70,31 @@ def partial():
 
 def lim():
     try:
-        x = Symbol('x')
-        print('Enter general term:')
-        an = parseInput('sym')
-        result = limit(eval(an), x, oo)
-        print('Lim {0} = {1}'.format(an, result))
+        serie = ''
+        print('Enter the limit: ', end='')
+        t = input()
+        top = eval(t)
+        print('Enter general term(x): ', end='')
+        an = parseInput('math')
+        if isinstance(top, int):
+            for i in range(1, top + 1):
+                if i != 1:
+                    serie += ' + '
+                exp = re.sub('x', str(i), an)
+                expValue = calcualteFactorial(exp)
+                serie += str(eval(expValue))
+            print('Σ({0}) = {1} = {2}'.format(an, serie, eval(serie)))
+        elif t == 'oo':
+            i = 1
+            print('Σ({0}) ='.format(an), end='')
+            while True:
+                exp = re.sub('x', str(i), an)
+                expValue = calcualteFactorial(exp)
+                result = round(eval(expValue), 8)
+                print(' {0} +'.format(result), end='')
+                i += 1
+        else:
+            print('The limit of the series must be an integer or infinity')
     except:
         print('Misspelled equations')
     input("Press Enter to continue...")
@@ -84,8 +106,29 @@ def parseInput(prefix):
     cosValue = re.sub('cos', prefix + '.cos', sinValue)
     tanValue = re.sub('tan', prefix + '.tan', cosValue)
     sqrtValue = re.sub('sqrt', prefix + '.sqrt', tanValue)
-    parseValue = re.sub(r'\^', '**', sqrtValue)
+    if (prefix == 'sym'):
+        piValue = re.sub('pi', 'math.pi', sqrtValue)
+    else:
+        piValue = re.sub('pi', prefix + '.pi', sqrtValue)
+    facValue = calcualteFactorial(piValue)
+    parseValue = re.sub(r'\^', '**', facValue)
     return parseValue
+
+
+def calcualteFactorial(value):
+    try:
+        resultFactorial = value
+        searchFactorial = re.findall(r'\d+!', value)
+        for factorial in searchFactorial:
+            integer = re.search(r'\d+', factorial)
+            init = eval(integer.group(0))
+            result = 1
+            for i in range(1, init + 1):
+                result *= i
+            resultFactorial = re.sub(factorial, str(result), resultFactorial)
+        return resultFactorial
+    except:
+        print('Error calculating the factorial')
 
 
 try:
@@ -94,9 +137,9 @@ try:
         clear()
         print('1. Calculate if the field is conservative')
         print('2. Draw the field')
-        print('3. Calculate limit')
+        print('3. Calculate series')
         print('4. Go out')
-        print('Select option:')
+        print('Select option: ', end='')
         opt = input()
         if eval(opt) == 1:
             partial()
